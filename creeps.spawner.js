22952dd0c62dce55da39cd413ac7a49418313153
VitalyGorgun <gorgun97@gmail.com>
2022-07-30 20:59:10 +0200

    2

--- creeps.spawner.js
+++ creeps.spawner.js
@@ -1,36 +1,39 @@
 var creepsSpawner = {
     run: function (harvestersCount, upgradersCount, buildersCount) {
-        let rnd = Math.round(Math.random()*100000);
+        let rnd = Math.round(Math.random() * 100);
 
         if (harvestersCount < 5) {
-            let x = Game.spawns.SP.createCreep([WORK, WORK, MOVE, CARRY],'Harvester '+ rnd);
+            let x = Game.spawns.SP.createCreep([WORK, WORK, CARRY, CARRY, MOVE], 'H ' + rnd);
 
             for (let name in Game.creeps) {
-                if(!Game.creeps[name].memory.role){
+                if (!Game.creeps[name].memory.role) {
                     Game.creeps[name].memory.role = 'Harvester';
                 }
             }
         }
 
-        if (upgradersCount < 5) {
-            let x = Game.spawns.SP.createCreep([WORK, MOVE, MOVE, CARRY],'Upgrader '+ rnd);
+        if (upgradersCount < 3) {
+            let x = Game.spawns.SP.createCreep([WORK, WORK, CARRY, CARRY, MOVE], 'U ' + rnd);
 
             for (let name in Game.creeps) {
-                if(!Game.creeps[name].memory.role){
+                if (!Game.creeps[name].memory.role) {
                     Game.creeps[name].memory.role = 'Upgrader';
                 }
             }
         }
-        
+
         if (buildersCount < 5) {
-            let x = Game.spawns.SP.createCreep([WORK, WORK, MOVE, CARRY],'Builder '+ rnd);
+            let x = Game.spawns.SP.createCreep([WORK, WORK, CARRY, CARRY, MOVE], 'B ' + rnd);
 
             for (let name in Game.creeps) {
-                if(!Game.creeps[name].memory.role){
+                if (!Game.creeps[name].memory.role) {
                     Game.creeps[name].memory.role = 'Builder';
                 }
             }
         }
     }
 }
-module.exports = creepsSpawner;  
+module.exports = creepsSpawner;
+
+//WORK, MOVE, MOVE, CARRY
+//BODYPART_COST: { "move": 50, "work": 100, "attack": 80, "carry": 50, "heal": 250, "ranged_attack": 150, "tough": 10, "claim": 600 }
--- role.harvester.js
+++ role.harvester.js
@@ -1,16 +1,35 @@
 var roleHarvester = {
     run: function (creep) {
-            
-            if (creep.carry.energy < creep.carryCapacity) {
-                let sources = creep.room.find(FIND_SOURCES);
-                if (creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(sources[1]);
-                }
-            } else {
-                if (creep.transfer(Game.spawns.SP, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(Game.spawns.SP);
-                }
+
+        if (creep.carry.energy < creep.carryCapacity) {
+            let sources = creep.room.find(FIND_SOURCES);
+            if (creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[1]);
             }
+        } else {
+            structures = creep.room.find(FIND_MY_STRUCTURES, {
+                filter: function (object) {
+                    return object.energy < object.energyCapacity;;
+                }
+            });
+
+            if (creep.transfer(structures[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(structures[0]);
+
+                console.log(structures[0]);
+
+            }
+        }
     }
 }
 module.exports = roleHarvester;
+
+// structures = creep.room.find(FIND_MY_STRUCTURES, {
+//     filter: function (object) {
+//         return object.energy < object.energyCapacity;;
+//     }
+// });
+
+// if (ERR_NOT_IN_RANGE === creep.transferEnergy(structures[0])) {
+//     creep.moveTo(structures[0]);
+// }
