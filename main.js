2154c6353a9795c8c638adaf078ca6da4c682771
VitalyGorgun <gorgun97@gmail.com>
2022-07-30 19:26:56 +0200

    dsfasdf

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ module.exports.loop = function () {
             upgradersCount++;
         }
         if (creep.memory.role == 'Builder') {
-            roleBuilder.run(creep); 
+            roleBuilder.run(creep);  
             buildersCount++;
         }
 
